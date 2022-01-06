ce9982cfdc985d2851b692fcef7aab9673a3a43b
matthewesosa <67363297+matthewesosa@users.noreply.github.com>
2022-01-06 12:20:07 +0100

    Add files via upload

---
+++ dsp_lab2_11142135.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 14 07:12:14 2020
+
+@author: LENOVO
+"""
+from thkdsp.dsplab.lab2 import *
+from thkdsp.audio import *
+from numpy import *
+start_dsp_lab2()
